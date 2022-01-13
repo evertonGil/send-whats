4abd0b268908a79d2c6e32c263234562ac5cccfb
Everton Gil Matos dos Santos <everton.santos@dotzmkt.com.br>
2022-01-13 14:21:46 -0300

    refactor: adding signIn behavior

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy, ElementRef, ViewChild } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { catchError, throwError } from "rxjs";
 import { AuthService } from "src/app/services/auth.service";
 
 @Component({
@@ -14,15 +16,18 @@ export class LoginComponent implements OnInit, OnDestroy {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
-  employeeDetailsForm = this.fb.group({
-    email: [''],
-    password: ['']
+  messageApiError: string;
+
+  loginForm = this.fb.group({
+    login: ['', Validators.required],
+    password: ['', Validators.required]
   });
-  
-  get passwordInputType () {
+
+  get passwordInputType() {
     const passwordInput = this.passwordInput?.nativeElement;
     return passwordInput?.type;
   }
@@ -36,11 +41,11 @@ export class LoginComponent implements OnInit, OnDestroy {
     return this.passwordInputType == 'text' ? 'far fa-eye-slash' : 'far fa-eye';
   }
 
-  ngOnInit() {}
-  ngOnDestroy() {}
+  ngOnInit() { }
+  ngOnDestroy() { }
 
   tooglePassword() {
-    if(this.passwordInputType == "password"){
+    if (this.passwordInputType == "password") {
       this.passwordInputType = "text";
     } else {
       this.passwordInputType = "password";
@@ -49,12 +54,27 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   signIn(event: Event) {
     event.preventDefault();
-    
-    console.log('Login')
-    this.authService.signIn().subscribe((res) => {
-      if (res) {
-        this.router.navigate(["/dashboard"]);
-      }
-    });
+
+    if (this.loginForm.valid) {
+      console.log('Login');
+
+      const { login, password } = this.loginForm.value;
+
+      this.authService.signIn({ login, password, role: 'admin' })
+        .pipe(catchError((err, _) => {
+          console.log('err', err);
+          if (err.error) {
+            this.messageApiError = err.error?.data?.message;
+            this.toastr.success('erro', err.error?.data?.message);
+          }
+          return throwError(err.message);
+        }))
+        .subscribe((res) => {
+          console.log('resposta login', res);
+          this.toastr.success('Hello world!', 'Toastr fun!');
+
+          this.router.navigate(["/dashboard"]);
+        });
+    }
   }
 }
