d8ee2aac9c143b4b5d066c47ff3ed316acf8d8d6
Everton Gil Matos dos Santos <everton.santos@dotzmkt.com.br>
2022-01-13 23:59:14 -0300

    refactor: formating register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'dsw-register',
@@ -7,11 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  registerForm = this.fb.group({
+    name: ['', Validators.required],
+    lastName: ['', Validators.required],
+    docNumber: ['', Validators.required],
+    docType: ['', Validators.required],
+    email: ['', Validators.required],
+    address: this.fb.group({
+      address: ['', Validators.required],
+      district: ['', Validators.required],
+      uf: ['', Validators.required],
+      number: ['', Validators.required],
+      status: ['', Validators.required],
+      city: ['', Validators.required],
+      zipCode: ['', Validators.required],
+      country: ['', Validators.required],
+      complemento: ['', Validators.required]
+    }),
+    phone: [[], Validators.required, Validators.minLength(2)],
+    idUser: ['', Validators.required],
+    user: this.fb.group({
+      password: ['', Validators.required],
+      passwordAgain: ['', Validators.required]
+    })
+  });
 
   passwordStrength = 'fraca';
 
   ngOnInit() {
   }
 
+  ngOnChanges() {
+  }
+
+  onSubmit(event: Event) {
+
+    event.preventDefault();
+
+    console.log(this.registerForm.value);
+  }
 }
